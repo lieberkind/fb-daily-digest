42c4f5317720c4a6a1d3338baa8cf2ed39f44d77
Tomas Lieberkind <t.lieberkind@gmail.com>
2016-02-21 23:29:56 +0100

    Changed styling of html

--- dailydigest.js
+++ dailydigest.js
@@ -14,9 +14,7 @@ var MAILGUN_URI = "https://api.mailgun.net/v3";
                 return moment(entry.published).isAfter(oneDayAgo);
             });
 
-            var html = newEntries.reduce(function(html, entry) {
-                return html + entryToHtml(entry);
-            }, '');
+            var html = generateEmailTemplate(newEntries);
 
             sendMail('t.lieberkind@gmail.com', 't.lieberkind@gmail.com', 'FB Daily Digest', html)
                 .then(function() {
@@ -25,15 +23,36 @@ var MAILGUN_URI = "https://api.mailgun.net/v3";
         });
 }());
 
+function generateEmailTemplate(entries) {
+    var from = moment().subtract(1, 'days').format('dddd DD/M');
+    var to = moment().format('dddd DD/M');
+
+    var html = '';
+    html += '<div style="font-family: &quot;Monaco&quot;;font-size: 14px;line-height: 1.5em;margin: 0 auto;max-width: 600px;">';
+    html += '<h1 style="font-size: 1.2em;font-weight: bold;margin-bottom: 3em;">Notifications from ' + from + ' to ' + to + '</h1>';
+    html += entries.reduce(function(html, entry) {
+        return html + entryToHtml(entry);
+    }, '');
+    html += '</div>';
+
+    return html;
+}
+
 function entryToHtml(entry) {
+    var published = moment(entry.published);
+
     var html = '';
 
-    html += '<div>';
-    html += '<h2>';
-    html += '<a href="' + entry.alternate + '">' + entry.title + '</a>';
-    html += '</h2>';
-    html += '<div>';
+    html += '<div class="entry" style="overflow: hidden;margin-bottom: 2em;">';
+
+    html += '<div class="timestamp" style="box-sizing: border-box;float: left;width: 180px;">';
+    html += '<a href="' + entry.alternate + '" style="color: #3a5795;">' + published.format('dddd') + ' at ' + published.format('H:mm') + '</a>';
+    html += '</div>';
+
+    html += '<div class="notification" style="box-sizing: border-box;float: left;width: 420px;">';
     html += entry.content;
+    html += '</div>';
+
     html += '</div>';
 
     return html;
