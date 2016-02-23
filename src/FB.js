cc91fe121ab728b52f9ddc66acda920cf445aca7
Tomas Lieberkind <t.lieberkind@gmail.com>
2016-02-23 21:43:22 +0100

    More functional

--- src/FB.js
+++ src/FB.js
@@ -4,13 +4,15 @@ var Q = require('q');
 var request = require('request');
 var Helpers = require('./Helpers');
 
-var FEED_URI = 'https://www.facebook.com/feeds/notifications.php';
+const FEED_URI = 'https://www.facebook.com/feeds/notifications.php';
 
+// isEntryAfter :: Moment -> Entry -> Boolean
 var isEntryAfter = _.curry(function(since, entry) {
     return moment(entry.published).isAfter(since);
 });
 
-var notifications = _.curry(function(fbId, feedKey, since) {
+// getNotifications :: String -> String -> Moment -> Promise [Entries]
+var getNotifications = _.curry(function(fbId, feedKey, since) {
     var options = {
         url: FEED_URI,
         qs: {
@@ -27,10 +29,12 @@ var notifications = _.curry(function(fbId, feedKey, since) {
     return Helpers.httpRequest(options)
         .get('entries')
         .then(function(entries) {
-            return since ? _.filter(isEntryAfter(since), entries) : entries;
+            return since ?
+                _.filter(isEntryAfter(since), entries) :
+                entries;
         });
 });
 
 module.exports = {
-    notifications: notifications
+    getNotifications: getNotifications
 };
--- src/Helpers.js
+++ src/Helpers.js
@@ -1,6 +1,7 @@
 var Q = require('q');
 var request = require('request');
 
+// httpRequest :: Object -> Promise
 var httpRequest = function(options) {
     return promise = Q.promise(function(resolve, reject) {
         request(options, function(error, response, body) {
@@ -13,6 +14,7 @@ var httpRequest = function(options) {
     });
 };
 
+// print :: String -> _ -> _
 var print = function(text) {
     return function() {
         console.log(text);
